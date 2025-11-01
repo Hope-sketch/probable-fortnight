07951d3a7720cc9c8acc3433012ce24197f23eb6
Njabulo Hope Makana <makanahopenjabulo@gmail.com>
2025-11-01 14:23:32 +0200

    Add Hello World C# program

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using System; 
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print a message to the console
+            Console.WriteLine("Hello, World!");
+
+            
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
